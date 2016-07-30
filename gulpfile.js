c14585d1627817295ef8ff7f6d270a0e4fdbea3b
Alan Orth <alan.orth@gmail.com>
2016-07-30 18:42:54 +0300

    gulpfile.js: Compile bootstrap from sass
    Later I will need to customize it so it's better to just start
    using the SASS version now.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
-gulp.task('default', ['css', 'js']);
+gulp.task('default', ['scss', 'js']);
 
 gulp.task('css', function() {
    return gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
@@ -12,4 +13,14 @@ gulp.task('css', function() {
 gulp.task('js', function() {
    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js'])
    .pipe(gulp.dest('./js'));
+});
+
+gulp.task('scss', function () {
+   return gulp.src('scss/style.scss')
+          .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+          .pipe(gulp.dest('css'));
+});
+
+gulp.task('scss:watch', function () {
+  gulp.watch('./scss/*.scss', ['scss']);
 });

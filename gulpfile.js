beec338c999e1bcdfa312e43ba9c664138de3099
Alan Orth <alan.orth@gmail.com>
2016-07-30 19:05:46 +0300

    gulpfile.js: Rename css task to style

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,20 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     cleanCSS = require('gulp-clean-css');
 
-gulp.task('default', ['scss', 'js']);
+gulp.task('default', ['style', 'js']);
 
 gulp.task('js', function() {
    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js'])
    .pipe(gulp.dest('./js'));
 });
 
-gulp.task('scss', function () {
+gulp.task('style', function () {
    return gulp.src('scss/style.scss')
           .pipe(sass().on('error', sass.logError))
           .pipe(cleanCSS({compatibility: 'ie9'}))
           .pipe(gulp.dest('css'));
 });
 
-gulp.task('scss:watch', function () {
-  gulp.watch('./scss/*.scss', ['scss']);
+gulp.task('style:watch', function () {
+  gulp.watch('./scss/*.scss', ['style']);
 });

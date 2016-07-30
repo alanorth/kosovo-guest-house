69c18c4997cad7beccfc9316a670c71f258faf98
Alan Orth <alan.orth@gmail.com>
2016-07-30 19:00:58 +0300

    gulpfile.js: Remove css task
    Deprecated in favor of buildng bootstrap's SASS directly using the
    scss task.

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,6 @@ var gulp = require('gulp'),
 
 gulp.task('default', ['scss', 'js']);
 
-gulp.task('css', function() {
-   return gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
-   .pipe(gulp.dest('./css'));
-});
-
 gulp.task('js', function() {
    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js'])
    .pipe(gulp.dest('./js'));
